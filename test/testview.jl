d3b4d2c1e0730c3c46aeee2b0a56b5771fce493e
Dylan Festa <darksnail@gmail.com>
2021-10-19 19:35:46 +0200

    working on correct simulations with noise

--- test/testview.jl
+++ test/testview.jl
@@ -39,6 +39,40 @@ cov_noise_test = Symmetric(diagm(rand(Uniform(2.2,3.3),ntot)))
 ntw = S.RecurrentNeuralNetwork(ne,ni;sigma_noise=cov_noise_test,
   iofunction=S.ReLu(0.05))
 
+ntw.weight_matrix .= 0.0
+ntw.base_input .= 0.0
+
+mu_start,sigma_out = S.mustart_sigmastart(ntw)
+t,ei_v,ei=S.run_network_noise(ntw,mu_start,20.;verbose=true,stepsize=0.01)
+sigma_num = cov(ei_v;dims=2)
+v_sim_out = median(ei_v;dims=2)[:]
+
+sigma_num
+
+dSigma_alloc = similar(sigma_out)
+
+global const ε = 1E-3
+for i in 1:10_000
+  S.dSigma!(dSigma_alloc,sigma_out,v_sim_out,ntw)
+  sigma_out .+= ε*dSigma_alloc
+end
+
+plotvs(sigma_num,sigma_out)
+plotvs(sigma_num, 37.22 .* sigma_out)
+
+median(sigma_num./sigma_out ) 
+
+
+
+
+
+##
+ne,ni = 53,31
+ntot = ne+ni
+cov_noise_test = Symmetric(diagm(rand(Uniform(2.2,3.3),ntot)))
+ntw = S.RecurrentNeuralNetwork(ne,ni;sigma_noise=cov_noise_test,
+  iofunction=S.ReLu(0.05))
+
 # more weight variability
 wscal = rand(Uniform(1.7,3.3),S.n_neurons(ntw))
 broadcast!(*,ntw.weight_matrix,ntw.weight_matrix,wscal)
@@ -89,6 +123,6 @@ for i in 1:10_000
 end
 
 plotvs(mu_an, mu_num)
-plotvs(sigma_an,0.06538 .* sigma_num)
+plotvs(sigma_an,sigma_num)
 
 mean(sigma_an ./ sigma_num)
